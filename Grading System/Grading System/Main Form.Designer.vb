c2815128bd470da5bdc3895367c488f6c4b0d9d4
mkmaupin <44644766+mkmaupin@users.noreply.github.com>
2018-11-27 13:17:58 -0600

    Testing testing 123

--- Grading System/Grading System/Main Form.Designer.vb
+++ Grading System/Grading System/Main Form.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmMain
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,18 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(749, 365)
+        Me.Name = "frmMain"
+        Me.ShowIcon = False
+        Me.Text = "Life Prep Academy"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
--- Grading System/Grading System/My Project/Application.Designer.vb
+++ Grading System/Grading System/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Grading_System.Form1
+            Me.MainForm = Global.Grading_System.frmMain
         End Sub
     End Class
 End Namespace
