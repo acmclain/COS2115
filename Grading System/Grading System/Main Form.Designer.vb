368d536dd2612f23c75cd2cf0fd9354aa49c5654
Hannah <hdruple@students.unwsp.edu>
2018-11-27 14:14:18 -0600

    oof

--- Grading System/Grading System/Main Form.Designer.vb
+++ Grading System/Grading System/Main Form.Designer.vb
@@ -33,10 +33,9 @@ Partial Class frmMain
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(618, 311)
-        Me.btnExit.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnExit.Location = New System.Drawing.Point(412, 202)
         Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(112, 35)
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
         Me.btnExit.TabIndex = 1
         Me.btnExit.Text = "Exit"
         Me.btnExit.UseVisualStyleBackColor = True
@@ -45,17 +44,19 @@ Partial Class frmMain
         '
         Me.lblStudentID.BackColor = System.Drawing.Color.Lavender
         Me.lblStudentID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblStudentID.Location = New System.Drawing.Point(59, 54)
+        Me.lblStudentID.Location = New System.Drawing.Point(40, 37)
+        Me.lblStudentID.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblStudentID.Name = "lblStudentID"
-        Me.lblStudentID.Size = New System.Drawing.Size(100, 23)
+        Me.lblStudentID.Size = New System.Drawing.Size(67, 16)
         Me.lblStudentID.TabIndex = 2
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(55, 24)
+        Me.Label1.Location = New System.Drawing.Point(37, 16)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(91, 20)
+        Me.Label1.Size = New System.Drawing.Size(61, 13)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Student ID:"
         '
@@ -63,26 +64,29 @@ Partial Class frmMain
         '
         Me.blbFirst.BackColor = System.Drawing.Color.Lavender
         Me.blbFirst.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.blbFirst.Location = New System.Drawing.Point(237, 57)
+        Me.blbFirst.Location = New System.Drawing.Point(158, 37)
+        Me.blbFirst.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.blbFirst.Name = "blbFirst"
-        Me.blbFirst.Size = New System.Drawing.Size(100, 23)
+        Me.blbFirst.Size = New System.Drawing.Size(67, 16)
         Me.blbFirst.TabIndex = 4
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(233, 24)
+        Me.Label2.Location = New System.Drawing.Point(155, 16)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(90, 20)
+        Me.Label2.Size = New System.Drawing.Size(60, 13)
         Me.Label2.TabIndex = 5
         Me.Label2.Text = "First Name:"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(426, 24)
+        Me.Label3.Location = New System.Drawing.Point(284, 16)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(90, 20)
+        Me.Label3.Size = New System.Drawing.Size(61, 13)
         Me.Label3.TabIndex = 6
         Me.Label3.Text = "Last Name:"
         '
@@ -90,16 +94,17 @@ Partial Class frmMain
         '
         Me.lblLast.BackColor = System.Drawing.Color.Lavender
         Me.lblLast.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblLast.Location = New System.Drawing.Point(430, 57)
+        Me.lblLast.Location = New System.Drawing.Point(287, 37)
+        Me.lblLast.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblLast.Name = "lblLast"
-        Me.lblLast.Size = New System.Drawing.Size(100, 23)
+        Me.lblLast.Size = New System.Drawing.Size(67, 16)
         Me.lblLast.TabIndex = 7
         '
         'frmMain
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(748, 365)
+        Me.ClientSize = New System.Drawing.Size(499, 237)
         Me.Controls.Add(Me.lblLast)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
@@ -107,6 +112,7 @@ Partial Class frmMain
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.lblStudentID)
         Me.Controls.Add(Me.btnExit)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "frmMain"
         Me.ShowIcon = False
         Me.Text = "Life Prep Academy"
--- Grading System/Grading System/Main Form.vb
+++ Grading System/Grading System/Main Form.vb
@@ -2,16 +2,4 @@
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
     End Sub
-
-    Private Sub picLPA_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
-
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
-
-    End Sub
 End Class
