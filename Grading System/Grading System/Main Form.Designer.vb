38c45b0dd07dafd373849ac70be407edc40af820
mkmaupin <44644766+mkmaupin@users.noreply.github.com>
2018-11-27 14:13:00 -0600

    pretty

--- Grading System/Grading System/Main Form.Designer.vb
+++ Grading System/Grading System/Main Form.Designer.vb
@@ -23,6 +23,12 @@ Partial Class frmMain
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblStudentID = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.blbFirst = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblLast = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'btnExit
@@ -35,17 +41,84 @@ Partial Class frmMain
         Me.btnExit.Text = "Exit"
         Me.btnExit.UseVisualStyleBackColor = True
         '
+        'lblStudentID
+        '
+        Me.lblStudentID.BackColor = System.Drawing.Color.Lavender
+        Me.lblStudentID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblStudentID.Location = New System.Drawing.Point(59, 54)
+        Me.lblStudentID.Name = "lblStudentID"
+        Me.lblStudentID.Size = New System.Drawing.Size(100, 23)
+        Me.lblStudentID.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(55, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Student ID:"
+        '
+        'blbFirst
+        '
+        Me.blbFirst.BackColor = System.Drawing.Color.Lavender
+        Me.blbFirst.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.blbFirst.Location = New System.Drawing.Point(237, 57)
+        Me.blbFirst.Name = "blbFirst"
+        Me.blbFirst.Size = New System.Drawing.Size(100, 23)
+        Me.blbFirst.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(233, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "First Name:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(426, 24)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(90, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Last Name:"
+        '
+        'lblLast
+        '
+        Me.lblLast.BackColor = System.Drawing.Color.Lavender
+        Me.lblLast.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblLast.Location = New System.Drawing.Point(430, 57)
+        Me.lblLast.Name = "lblLast"
+        Me.lblLast.Size = New System.Drawing.Size(100, 23)
+        Me.lblLast.TabIndex = 7
+        '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(748, 365)
+        Me.Controls.Add(Me.lblLast)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.blbFirst)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblStudentID)
         Me.Controls.Add(Me.btnExit)
         Me.Name = "frmMain"
         Me.ShowIcon = False
         Me.Text = "Life Prep Academy"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents btnExit As Button
+    Friend WithEvents lblStudentID As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents blbFirst As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblLast As Label
 End Class
--- Grading System/Grading System/Main Form.vb
+++ Grading System/Grading System/Main Form.vb
@@ -6,4 +6,12 @@
     Private Sub picLPA_Click(sender As Object, e As EventArgs)
 
     End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
 End Class
