7612764c894dfd338e4f72154b002c9108f647ab
acmclain <44502691+acmclain@users.noreply.github.com>
2018-11-29 13:29:30 -0600

    update to database connect

--- Grading System/Grading System/Main Form.vb
+++ Grading System/Grading System/Main Form.vb
@@ -8,10 +8,13 @@ Public Class frmMain
     End Sub
 
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'Hannah = MIS-W10-030\SQLEXPRESS
-        'Andrew = DESKTOP-6VVI9NG\SQLEXPRESS
 
+        'Mercy
         Dim connectionString As String = "Data Source = DESKTOP-II3CLP1\SQLEXPRESS;Initial Catalog=LPA;Integrated Security=true"
+        'Hannah
+        ' Dim connectionString As String = "Data Source = MIS-W10-030\SQLEXPRESS;Initial Catalog=LPA;Integrated Security=true"
+        'Andrew
+        '  Dim connectionString As String = "Data Source = DESKTOP-6VVI9NG\SQLEXPRESS;Initial Catalog=LPA;Integrated Security=true"
 
         Using connection As New SqlConnection(connectionString)
             connection.Open()
