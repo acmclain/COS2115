7cf1a9796bab95bcd9deb2637e6b0ddc9d31458d
acmclain <44502691+acmclain@users.noreply.github.com>
2018-12-04 13:51:50 -0600

    Select student

--- Grading System/Grading System/Main Form.vb
+++ Grading System/Grading System/Main Form.vb
@@ -4,38 +4,35 @@ Imports System.Data.SqlClient
 
 Public Class frmMainMenu
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
-
-        ' connection.close()
         Me.Close()
 
     End Sub
 
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        'Using connection As New SqlConnection(connectionString)
-        '  connection.Open()
-        '  MessageBox.Show("Database is open")
+        Using connection As New SqlConnection(connectionString)
+            connection.Open()
 
-        ' connection.Close()
-        ' MessageBox.Show("Database is closed")
+            Dim queryString As String = "SELECT FirstName, LastName FROM dbo.Students"
 
-        '  End Using
+            Dim command As New SqlCommand(queryString, connection)
+
+
+            Dim dataReader As SqlDataReader
+            dataReader = command.ExecuteReader()
+            While dataReader.Read
+                ' Write code to insert an Item into dropdownlist
+                cboStudents.Items.Add(dataReader("FirstName").ToString() & dataReader("LastName").ToString())
+            End While
+            connection.Close()
+            'cboStudents.Items.AddRange({command})
+
+        End Using
     End Sub
 
-    Private Sub btnDisplay_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs)
-
-    End Sub
 
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
         frmAddStudent.Show()
-
-    End Sub
-
-    Private Sub cboStudents_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboStudents.SelectedIndexChanged
 
     End Sub
 End Class
--- Grading System/Grading System/Student.vb
+++ Grading System/Grading System/Student.vb
@@ -1,7 +1,4 @@
 ﻿Public Class frmStudent
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
